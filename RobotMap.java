5819471d43e82cf252e30afc4af25d5f4832efd2
Ilya <fominiv10@gmail.com>
2023-02-26 02:23:19 +0300

    end commit

---
+++ RobotMap.java
@@ -0,0 +1,113 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RobotMap {
+
+    private final int n;
+    private final int m;
+    private final int length;
+
+    private List<Robot> robots;
+
+    public RobotMap(int n, int m, int length) {
+        this.n = n;
+        this.m = m;
+        this.length = length;
+        validateMap(n, m);
+        this.robots = new ArrayList<>();
+    }
+
+    public RobotMap(int n, int m) {
+        this(n, m, 5);
+        validateMap(n, m);
+        this.robots = new ArrayList<>();
+    }
+
+    public Robot createRobot(Point point) {
+        validatePoint(point);
+        Robot robot = new Robot(point);
+        robots.add(robot);
+        validateAmount(robots, length);
+
+        return robot;
+    }
+
+    private void validateAmount(List<Robot> arr, int length) {
+        if (arr.size() > length) {
+            throw new IllegalStateException("Вы не можете использовать больше " + length + " роботов!");
+        }
+    }
+
+    private void validateMap(int n, int m) {
+        if (n < 1 || m < 1) {
+            throw new IllegalStateException("Координаты поля не могут быть нулевыми или отрицательными!");
+        }
+    }
+
+    private void validatePoint(Point point) {
+        validatePointIsCorrect(point);
+        validatePointIsFree(point);
+    }
+
+    private void validatePointIsCorrect(Point point) {
+        if (point.x() < 0 || point.x() > n || point.y() < 0 || point.y() > m) {
+            throw new IllegalStateException("Некоректное значение точки!");
+        }
+    }
+
+    private void validatePointIsFree(Point point) {
+        for (Robot robot : robots) {
+            Point robotPoint = robot.point;
+            if (robotPoint.equals(point)) {
+                throw new IllegalStateException("Точка " + point + " занята!");
+            }
+        }
+    }
+
+    public class Robot {
+
+        public static final Direction DEFAULT_DIRECTION = Direction.TOP;
+
+        private Direction direction;
+        private Point point;
+
+        public Robot(Point point) {
+            this.direction = DEFAULT_DIRECTION;
+            this.point = point;
+        }
+
+        public void changeDirection(Direction direction) {
+            this.direction = direction;
+        }
+
+        public void move() {
+            Point newPoint = switch (Direction direction) {
+                case TOP -> new Point(point.x() - 1, point.y());
+                case RIGHT -> new Point(point.x(), point.y() + 1);
+                case BOTTOM -> new Point(point.x() + 1, point.y());
+                case LEFT -> new Point(point.x(), point.y() - 1);
+                default -> throw new IllegalArgumentException("Unexpected value: " + Direction);
+            };
+            validatePoint(newPoint);
+
+            System.out.println("Робот переместился с " + point + " на " + newPoint);
+            this.point = newPoint;
+        }
+
+        public void move(int steps) {
+            int i = 0;
+            while (i < steps) {
+                move();
+                i++;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return point.toString() + ", [" + direction.name() + "]";
+        }
+    }
+
+}
